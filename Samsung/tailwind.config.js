ed36bb2acf44fe6ea8d3655aa8ae7179eed4bda1
Varintorn Wattanajirapart <varintorn.m@gmail.com>
2023-06-11 17:53:36 +0700

    add samsung first draft

---
+++ Samsung/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'tv-stereo': "url('7.avif')",
+      }
+    },
+  },
+  plugins: [],
+}
+
